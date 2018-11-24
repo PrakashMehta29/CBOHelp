c3182fb8e2d98ded1ece193e77b2a9a61600e5bc
prakashmehta@97 <prakash.mehta2912@gmail.com>
2018-11-24 14:58:59 +0530

    2

---
+++ app/src/main/java/com/example/pc24/cbohelp/Followingup/mFollowupgrid.java
@@ -0,0 +1,273 @@
+package com.example.pc24.cbohelp.Followingup;
+
+public class mFollowupgrid {
+
+
+    private String sRNO;
+    private String pANAME;
+    private String cITY;
+    private String mobile;
+    private String person;
+    private String eMAIL;
+    private String cOMPANYTYPE;
+    private String uSER1ID;
+    private String uSERID;
+    private String nOOFEMPLOYEE;
+    private String rEFBY;
+    private String uSERNAME;
+    private String iD;
+    private String pAID;
+    private String sRNO1;
+    private String fOLLOWUPDATE;
+    private String fREMARK;
+    private String nEXTFOLLOWUPDATE;
+    private String cONTACTPERSON;
+    private String cONTACTNO;
+    private String fORMTYPE;
+    private String aUSERID;
+    private String sTATUS;
+    private String sTATUSDATE;
+    private String sTATUSREMARK;
+    private String uSER1NAME;
+
+    public mFollowupgrid() {
+
+    }
+
+    public mFollowupgrid(String sRNO, String pANAME, String cITY, String mobile, String person, String eMAIL, String cOMPANYTYPE, String uSER1ID, String uSERID, String nOOFEMPLOYEE, String rEFBY, String uSERNAME, String iD, String pAID, String sRNO1, String fOLLOWUPDATE, String fREMARK, String nEXTFOLLOWUPDATE, String cONTACTPERSON, String cONTACTNO, String fORMTYPE, String aUSERID, String sTATUS, String sTATUSDATE, String sTATUSREMARK, String uSER1NAME) {
+        this.sRNO = sRNO;
+        this.pANAME = pANAME;
+        this.cITY = cITY;
+        this.mobile = mobile;
+        this.person = person;
+        this.eMAIL = eMAIL;
+        this.cOMPANYTYPE = cOMPANYTYPE;
+        this.uSER1ID = uSER1ID;
+        this.uSERID = uSERID;
+        this.nOOFEMPLOYEE = nOOFEMPLOYEE;
+        this.rEFBY = rEFBY;
+        this.uSERNAME = uSERNAME;
+        this.iD = iD;
+        this.pAID = pAID;
+        this.sRNO1 = sRNO1;
+        this.fOLLOWUPDATE = fOLLOWUPDATE;
+        this.fREMARK = fREMARK;
+        this.nEXTFOLLOWUPDATE = nEXTFOLLOWUPDATE;
+        this.cONTACTPERSON = cONTACTPERSON;
+        this.cONTACTNO = cONTACTNO;
+        this.fORMTYPE = fORMTYPE;
+        this.aUSERID = aUSERID;
+        this.sTATUS = sTATUS;
+        this.sTATUSDATE = sTATUSDATE;
+        this.sTATUSREMARK = sTATUSREMARK;
+        this.uSER1NAME = uSER1NAME;
+    }
+
+    public String getsRNO() {
+        return sRNO;
+    }
+
+    public void setsRNO(String sRNO) {
+        this.sRNO = sRNO;
+    }
+
+    public String getpANAME() {
+        return pANAME;
+    }
+
+    public void setpANAME(String pANAME) {
+        this.pANAME = pANAME;
+    }
+
+    public String getcITY() {
+        return cITY;
+    }
+
+    public void setcITY(String cITY) {
+        this.cITY = cITY;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public String geteMAIL() {
+        return eMAIL;
+    }
+
+    public void seteMAIL(String eMAIL) {
+        this.eMAIL = eMAIL;
+    }
+
+    public String getcOMPANYTYPE() {
+        return cOMPANYTYPE;
+    }
+
+    public void setcOMPANYTYPE(String cOMPANYTYPE) {
+        this.cOMPANYTYPE = cOMPANYTYPE;
+    }
+
+    public String getuSER1ID() {
+        return uSER1ID;
+    }
+
+    public void setuSER1ID(String uSER1ID) {
+        this.uSER1ID = uSER1ID;
+    }
+
+    public String getuSERID() {
+        return uSERID;
+    }
+
+    public void setuSERID(String uSERID) {
+        this.uSERID = uSERID;
+    }
+
+    public String getnOOFEMPLOYEE() {
+        return nOOFEMPLOYEE;
+    }
+
+    public void setnOOFEMPLOYEE(String nOOFEMPLOYEE) {
+        this.nOOFEMPLOYEE = nOOFEMPLOYEE;
+    }
+
+    public String getrEFBY() {
+        return rEFBY;
+    }
+
+    public void setrEFBY(String rEFBY) {
+        this.rEFBY = rEFBY;
+    }
+
+    public String getuSERNAME() {
+        return uSERNAME;
+    }
+
+    public void setuSERNAME(String uSERNAME) {
+        this.uSERNAME = uSERNAME;
+    }
+
+    public String getiD() {
+        return iD;
+    }
+
+    public void setiD(String iD) {
+        this.iD = iD;
+    }
+
+    public String getpAID() {
+        return pAID;
+    }
+
+    public void setpAID(String pAID) {
+        this.pAID = pAID;
+    }
+
+    public String getsRNO1() {
+        return sRNO1;
+    }
+
+    public void setsRNO1(String sRNO1) {
+        this.sRNO1 = sRNO1;
+    }
+
+    public String getfOLLOWUPDATE() {
+        return fOLLOWUPDATE;
+    }
+
+    public void setfOLLOWUPDATE(String fOLLOWUPDATE) {
+        this.fOLLOWUPDATE = fOLLOWUPDATE;
+    }
+
+    public String getfREMARK() {
+        return fREMARK;
+    }
+
+    public void setfREMARK(String fREMARK) {
+        this.fREMARK = fREMARK;
+    }
+
+    public String getnEXTFOLLOWUPDATE() {
+        return nEXTFOLLOWUPDATE;
+    }
+
+    public void setnEXTFOLLOWUPDATE(String nEXTFOLLOWUPDATE) {
+        this.nEXTFOLLOWUPDATE = nEXTFOLLOWUPDATE;
+    }
+
+    public String getcONTACTPERSON() {
+        return cONTACTPERSON;
+    }
+
+    public void setcONTACTPERSON(String cONTACTPERSON) {
+        this.cONTACTPERSON = cONTACTPERSON;
+    }
+
+    public String getcONTACTNO() {
+        return cONTACTNO;
+    }
+
+    public void setcONTACTNO(String cONTACTNO) {
+        this.cONTACTNO = cONTACTNO;
+    }
+
+    public String getfORMTYPE() {
+        return fORMTYPE;
+    }
+
+    public void setfORMTYPE(String fORMTYPE) {
+        this.fORMTYPE = fORMTYPE;
+    }
+
+    public String getaUSERID() {
+        return aUSERID;
+    }
+
+    public void setaUSERID(String aUSERID) {
+        this.aUSERID = aUSERID;
+    }
+
+    public String getsTATUS() {
+        return sTATUS;
+    }
+
+    public void setsTATUS(String sTATUS) {
+        this.sTATUS = sTATUS;
+    }
+
+    public String getsTATUSDATE() {
+        return sTATUSDATE;
+    }
+
+    public void setsTATUSDATE(String sTATUSDATE) {
+        this.sTATUSDATE = sTATUSDATE;
+    }
+
+    public String getsTATUSREMARK() {
+        return sTATUSREMARK;
+    }
+
+    public void setsTATUSREMARK(String sTATUSREMARK) {
+        this.sTATUSREMARK = sTATUSREMARK;
+    }
+
+    public String getuSER1NAME() {
+        return uSER1NAME;
+    }
+
+    public void setuSER1NAME(String uSER1NAME) {
+        this.uSER1NAME = uSER1NAME;
+    }
+}
